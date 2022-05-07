2d01cddcae1bbd8320035ff57b4edd714685237b
Sanjid <sanjidk24@gmail.com>
2022-05-07 17:25:22 +0600

    delete item section done

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.port || 5000;
 
-
+// middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse-management.sjnu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
-    const itemsCollection = client.db('warehouse-management').collection('item');
+    const itemsCollection = client
+      .db("warehouse-management")
+      .collection("item");
 
-    app.get('/inventory', async (req, res) => {
+    app.get("/inventory", async (req, res) => {
       const query = {};
       const cursor = itemsCollection.find(query);
       const item = await cursor.toArray();
       res.send(item);
     });
 
-    
-    app.get('/inventory/:id' , async(req, res) => {
+    app.get("/inventory/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const item = await itemsCollection.findOne(query);
       res.send(item);
-  });
-  //POST
-  app.post('/inventory', async(req, res) => {
+    });
+
+    //POST
+    app.post("/inventory", async (req, res) => {
       const newItem = req.body;
       const result = await itemsCollection.insertOne(newItem);
-      res.send(result)
-  });
+      res.send(result);
+    });
 
-  //DELETE
-  app.delete('/inventory/:id', async(req, res) => {
+    //DELETE
+    app.delete("/inventory/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await itemsCollection.delete(query);
       res.send(result);
-  });
-
-
-  } 
-  finally {
+    });
+  } finally {
   }
 }
 run().catch(console.dir);
@@ -61,6 +63,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("listening to port", port);
 });
-
-
-
