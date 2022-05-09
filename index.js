61b17a88f860913dc619228cc72818289a2b0885
Sanjid <sanjidk24@gmail.com>
2022-05-09 23:40:58 +0600

    cors solve

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.port || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: true,
+  credentials: true,
+}
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig))
 app.use(express.json());
 
 
